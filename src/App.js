a40292e07b7e0ec175199eb567e95c3f2569ea58
viva-water <water@adoc.co.kr>
2021-03-02 10:04:18 +0900

    login commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LLLLLLLLLLLLLLLLLLLLLLLLLearn React
+          LLLLLLLLLLLLLLLLLLLLDLLLLLearn React
         </a>
       </header>
     </div>
